c342642acba6df1473098f990a62db8a7bbf067b
charles <codelyfewithcharlie@gmail.com>
2019-12-29 23:53:22 -0500

    added models folder

--- server.js
+++ server.js
@@ -12,24 +12,6 @@ mongoose.connect(DB, {
   useUnifiedTopology: true
 });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  }
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
 const { PORT } = process.env;
 
 app.listen(PORT, () => {
