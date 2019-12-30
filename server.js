dcbe89bdfb85868a31f7386606a8cd9ae007575b
charles <codelyfewithcharlie@gmail.com>
2019-12-29 23:21:35 -0500

    added mongo connection

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
+const mongoose = require('mongoose');
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+const DB = process.env.CONNECTION_STRING;
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const { PORT } = process.env;
 
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+  console.log('listening');
 });
